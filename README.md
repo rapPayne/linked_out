ca91bb270fd730c8a698a005ad662bd533a0430a
rapPayne <bob@creator.net>
2024-08-20 13:44:39 -0500

    Added people-fetcher process

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # linked_out
 Demo application for 2024 August Cohort
+
+## To load the database with random people
+```bash
+npm run load-db
+```
---
+++ make_random_people.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import fs from 'fs';
+
+const numberOfPeople: number = 100;
+const url: string = `https://randomuser.me/api?results=${numberOfPeople}&inc=name,location,email,cell,picture,dob`;
+
+const allThePeople: [] = await fetch(url)
+  .then(res => res.json())
+  .then(res => res.results)
+  .then(people => people.map((p, index) => ({ ...p, id: index + 10 })))
+  .catch(err => console.error("Error fetching people", err));
+
+const dbFileName = "./people.json";
+
+// If the file exists already, delete it.
+if (fs.existsSync(dbFileName)) {
+  fs.unlinkSync(dbFileName)
+}
+
+// Write this new data in JSON format to the file.
+fs.writeFileSync(dbFileName, JSON.stringify(allThePeople, null, 2))
+
+console.log(`database file (${dbFileName}) was created and seeded.`)
