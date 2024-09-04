7d44710f4a16faccfc689675785a655138218738
rapPayne <bob@creator.net>
2024-09-04 16:10:55 -0400

    Added JWT demonstration

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,23 +3,42 @@ import { Link, Route, Routes } from 'react-router-dom'
 import { Secret } from './components/Secret'
 import { Home } from './pages/Home';
 function App() {
-  const [loggedIn, setLogin] = useState(false);
+  const [user, setUser] = useState();
   return (
     <>
       <header>
         <Link to="/people">People</Link>
         <Link to="/secret">Secret</Link>
-        <button onClick={() => setLogin(loggedIn ? false : true)}>{loggedIn ? "Log out" : "Authenticate"}</button>
+        <button onClick={() => user ? logout() : login('rap', 'nunyabizness')}>{user ? "Log out" : "Authenticate"}</button>
       </header>
       <Routes>
         <Route path="/people" element={<Home />} />
-        <Route path="/secret" element={loggedIn ? <Secret /> : <h1>No dice!</h1>} />
+        <Route path="/secret" element={user ? <Secret /> : <h1>No dice!</h1>} />
       </Routes>
       <footer>
         <p>Copyright &copy; today us.com</p>
       </footer>
     </>
   )
+  function logout() {
+    setUser(undefined)
+  }
+  function login(username, password) {
+    const url = `http://localhost:3001/api/login`
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    })
+      .then(res => {
+        if (!res.ok) throw `Bad request: ${res.status}`;
+        console.log(`Logged in with this token: ${res.headers.get('Authorization')}`);
+        return res;
+      })
+      .then(res => res.json())
+      .then(u => setUser(u))
+  }
 }
+
 
 export default App
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import repo from './peopleMongoRepository.js';
+import jwt from 'jsonwebtoken';
+
 
 const { getAllPeople, getPerson, deletePerson } = repo;
 
@@ -9,7 +11,40 @@ const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use(express.static("client"));
+const jwtSecret = "This is the jwt-secret-key! Oooooooh!"
+
+//Middleware to process authentication via a JWT token
+app.use((req, res, next) => {
+  // check if client sent our JWT token in the 'authorization' header.
+  const authHeader = req.headers['authorization'];
+  const jwtToken = authHeader && authHeader.split(' ')[1];
+  // Get the username/data from the jwtToken and put it in req.user
+  jwt.verify(jwtToken, jwtSecret, (err, user) => {
+    if (err) {
+      console.warn("No user found")
+      return; // user isn't validated
+    }
+    req.user = user; // Now all other endpoints who the user is.
+  });
+  next();
+});
+
+//Endpoint added to demonstrate writing a JWT token.
+//POST /api/login
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  // Hardcoding a fake user. In a real scenario, this would be set by authenticating on login POST with a good username/password.
+  const user = { username, id: 733, first: "Shivani", last: "Kolanu" };
+  if (!user) {
+    res.send(401)
+  } else {
+    const jwtToken = jwt.sign(user, jwtSecret);
+    res.header('Authorization', `Bearer ${jwtToken}`);
+    res.send({ ...user, password: "***" }); // clobber the password in case it's part of the user record from the DB
+  }
+});
 
 //GET /api/people
 app.get('/api/people', async (req, res) => {
@@ -20,6 +55,7 @@ app.get('/api/people/:id', async (req, res) => {
   const { id } = req.params;
   const person = await getPerson(id);
   console.log("thePerson: ", person)
+  res.header('Authorization', `Bearer ${jwtToken}`); // Write JWT token to response
   if (person !== undefined)
     res.send(person);
   else
@@ -29,6 +65,7 @@ app.get('/api/people/:id', async (req, res) => {
 //TODO: move try/catch to the repository
 app.delete('/api/people/:id/', async (req, res) => {
   const { id } = req.params;
+  res.header('Authorization', `Bearer ${jwtToken}`); // Write JWT token to response
   try {
     let thePerson = await getPerson(+id);
     if (!thePerson) {
